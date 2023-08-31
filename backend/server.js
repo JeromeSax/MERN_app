deb586bd632fdee21536a9e0e5edf927c40935e6
unknown <jeromesloss@yahoo.com>
2023-08-31 09:24:33 -0400

    Goal controller and routes added

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 const app = express()
 
+// MIDDLEWARE
+// THIS MIDDLEWARE IS USED TO GET DATA
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// MIDDLEWARE
+
+
 // use goalRoutes to handle any endpoints that end with api
 app.use('/api/goals', require('./routes/goalRoutes'))
-// USER RESOURCE
-
-
+  
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
