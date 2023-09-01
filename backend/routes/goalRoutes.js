633fe52581fd16f08bef49292b9245408a1ed361
unknown <jeromesloss@yahoo.com>
2023-09-01 12:07:53 -0400

    Front end added

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,8 +7,10 @@ const {
     deleteGoal,
 } = require('../controllers/goalController')
 
+const {protect} = require('../middleware/authMiddleware')
+
 // GOALS ROUTES
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').delete(deleteGoal).put(updateGoal)
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
 
 module.exports = router;
